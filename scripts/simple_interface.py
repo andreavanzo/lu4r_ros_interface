9d1848e2348c8079dd518155c5f207c1886f814a
Andrea Vanzo <andrea.vanzo1@gmail.com>
2016-11-21 17:16:31 +0000

    fixes on the simple_interface semantic_map2 fixed

--- scripts/simple_interface.py
+++ scripts/simple_interface.py
@@ -6,8 +6,7 @@
 import socket
 import sys
 import requests
-import json
-from math import radians, sin, cos
+import os
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
@@ -17,14 +16,15 @@ host = ''
 port = 5001
 HEADERS = {'content-type': 'application/json'}
 
-def listener():
-	global semantic_map
+
+def listener(port, lu4r_ip, lu4r_port, semantic_map):
 	global conn
-	lu4r_ip = "127.0.0.1"
-	lu4r_port = "9090"
+	port = int(port)
 	lu4r_url = 'http://' + lu4r_ip + ':' + str(lu4r_port) + '/service/nlu'
-	entities = "{\"entities\":[]}"
-	currentFragment = ''
+	print lu4r_url
+	working_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	entities = open(working_dir + "/semantic_maps/" + semantic_map).read()
+	current_fragment = ''
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	print 'Socket created'
@@ -45,33 +45,37 @@ def listener():
 			print 'Received: '+data
 			if data and not data.isspace():
 				if "KEEP_AWAKE" in data:
-					data = data.replace("KEEP_AWAKE\n","")
+					data = data.replace("KEEP_AWAKE\n", "")
 					if len(data) == 0:
 						continue
 				if '$' in data:
-					currentFragment = data[1:-1]
+					current_fragment = data[1:-1]
 					continue				
-				if currentFragment == "HOME":
+				if current_fragment == "HOME":
 					continue				
-				elif currentFragment == "JOY":
+				elif current_fragment == "JOY":
 					rho_theta = data.split()
-					rho = float(rho_theta[0])
-					theta = radians(float(rho_theta[1]))
-					#motion.linear.x = max_tv * rho * sin(theta)
-					#motion.angular.z = max_rv * -1 * rho * cos(theta)
-					#v_joyopad.publish(motion)
-				elif currentFragment == "SLU":
-					toSend = {'hypo': data, 'entities': entities}
-					response = requests.post(lu4r_url, toSend, headers = HEADERS)
+				elif current_fragment == "SLU":
+					to_send = {'hypo': data, 'entities': entities}
+					response = requests.post(lu4r_url, to_send, headers=HEADERS)
 					print response.text
 				else:
 					print "Unknown service"
 			else:
 				print 'Disconnected from ' + addr[0] + ':' + str(addr[1])
-				currentFragment = ''
+				current_fragment = ''
 				break
 	s.close()
 
 if __name__ == '__main__':
-	listener()
+	for option in sys.argv[1:]:
+		if "_lu4r_port:=" in option:
+			lu4r_port = str(option).replace("_lu4r_port:=", "")
+		elif "_lu4r_ip:=" in option:
+			lu4r_ip = str(option).replace("_lu4r_ip:=", "")
+		elif "_port:=" in option:
+			port = str(option).replace("_port:=", "")
+		elif "_semantic_map:=" in option:
+			semantic_map = str(option).replace("_semantic_map:=", "")
+	listener(port, lu4r_ip, lu4r_port, semantic_map)
 
