1b0e249541c359e1d0054375f76d6e9f9dfb2b83
Andrea Vanzo <andrea.vanzo1@gmail.com>
2017-01-12 14:09:48 +0100

    minor fix

--- scripts/android_interface.py
+++ scripts/android_interface.py
@@ -56,6 +56,7 @@ def listener():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	print 'Socket created'
+	host = ''
 	try:
 		s.bind((host, listening_port))
 	except socket.error as msg:
